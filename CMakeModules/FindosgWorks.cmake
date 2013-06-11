16eaf1fab1e114b985ab9b598658b65e0e964898
Patrick Carlson <carlsonp@iastate.edu>
2013-06-11 11:02:31 -0500

    rewrite FindosgWorks.cmake so it looks for the appropriate libraries

--- CMakeModules/FindosgWorks.cmake
+++ CMakeModules/FindosgWorks.cmake
@@ -2,47 +2,51 @@
 # The following variables are set if osgWorks is found.  If osgWorks is not
 # found, osgWorks_FOUND is set to false.
 #  osgWorks_FOUND         - Set to true when osgWorks is found.
-#  osgWorks_USE_FILE      - CMake file to use osgWorks.
-#  osgWorks_MAJOR_VERSION - The osgWorks major version number.
-#  osgWorks_MINOR_VERSION - The osgWorks minor version number 
-#                       (odd non-release).
-#  osgWorks_BUILD_VERSION - The osgWorks patch level 
-#                       (meaningless for odd minor).
 #  osgWorks_INCLUDE_DIRS  - Include directories for osgWorks
 #  osgWorks_LIBRARY_DIRS  - Link directories for osgWorks libraries
 
-# The following cache entries must be set by the user to locate osgWorks:
-#  osgWorks_DIR  - The directory containing osgWorksConfig.cmake.  
-#             This is either the root of the build tree,
-#             or the lib directory.  This is the 
-#             only cache entry.
+# TODO: use Version.h header file and figure out exact version
 
-# In situations in which osgWorks will be built later and we want to override this CMake variable,
-# this check is necessary.
-IF(NOT osgWorks_FOUND)
-	# Assume not found.
-	SET(osgWorks_FOUND FALSE BOOL)
-ENDIF(NOT osgWorks_FOUND)
 
-# Construct consitent error messages for use below.
-SET(osgWorks_DIR_DESCRIPTION "directory containing osgWorksConfig.cmake.  This is either the root of the build tree, or PREFIX/lib for an installation.")
-SET(osgWorks_DIR_MESSAGE "osgWorks not found.  Set the osgWorks_DIR cmake cache entry to the ${osgWorks_DIR_DESCRIPTION}")
+set(osgWorks_FOUND false)
 
-# Use the Config mode of the find_package() command to find osgWorksConfig.
-# If this succeeds (possibly because osgWorks_DIR is already set), the
-# command will have already loaded osgWorksConfig.cmake and set osgWorks_FOUND.
-IF(NOT osgWorks_FOUND)
-  FIND_PACKAGE(osgWorks QUIET NO_MODULE)
-ENDIF(NOT osgWorks_FOUND)
+find_path(
+	osgWorks_INCLUDE_DIRS
+		osgwTools/Version.h
+	PATH_SUFFIXES
+		include
+	PATHS
+		/usr
+		/usr/local
+)
 
-#-----------------------------------------------------------------------------
-IF(NOT osgWorks_FOUND)
-  # osgWorks not found, explain to the user how to specify its location.
-  IF(osgWorks_FIND_REQUIRED)
-    MESSAGE(FATAL_ERROR ${osgWorks_DIR_MESSAGE})
-  ELSE(osgWorks_FIND_REQUIRED)
-    IF(NOT osgWorks_FIND_QUIETLY)
-      MESSAGE(STATUS ${osgWorks_DIR_MESSAGE})
-    ENDIF(NOT osgWorks_FIND_QUIETLY)
-  ENDIF(osgWorks_FIND_REQUIRED)
-ENDIF(NOT osgWorks_FOUND)
+set(osgWorks_Components osgwControls osgwMx osgwQuery osgwTools)
+
+foreach(component ${osgWorks_Components})
+	find_library(
+		osgWorks_${component}_LIB
+			${component}
+		PATH_SUFFIXES
+			lib
+			lib64
+		PATHS
+			/usr
+			/usr/local
+	)
+	if(osgWorks_${component}_LIB)
+		list(APPEND osgWorks_LIBRARY_DIRS ${osgWorks_${component}_LIB})
+	else()
+		message(FATAL_ERROR "osgWorks: Unable to find ${component}")
+	endif()
+	#Clear out the variable so it doesn't show up in CMake
+	unset(osgWorks_${component}_LIB CACHE)
+endforeach()
+
+
+if( NOT osgWorks_INCLUDE_DIRS OR NOT osgWorks_LIBRARY_DIRS )
+	message(FATAL_ERROR "osgworks not found.")
+else()
+	set(osgWorks_FOUND true)
+	message(STATUS "osgworks found: ${osgWorks_INCLUDE_DIRS}")
+	message(STATUS "osgworks libraries: ${osgWorks_LIBRARY_DIRS}") #Figure out why this variable doesn't show up in CMake GUI
+endif()
